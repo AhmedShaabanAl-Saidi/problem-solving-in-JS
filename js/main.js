2cc22f1850a1b183da1601bcd79f769051c70a4e
AhmedShaabanAl-Saidi <ahmedshaaban123321@gmail.com>
2024-08-08 20:29:16 +0300

    MakeUpperCase

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
-// Formatting decimal places
+// 1-Formatting decimal places
 function twoDecimals(num) {
   return Number(num.toFixed(2));
 }
 
 console.log(twoDecimals(5.8545));
+
+// 2-MakeUpperCase
+function upperCase(str) {
+  return str.toUpperCase();
+}
+
+console.log(upperCase("ahmed"));
